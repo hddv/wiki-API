659a556f9f59caaad1f27523f2cb8503033ad163
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-07-20 22:57:27 +0200

    setup server mongoose

--- app.js
+++ app.js
@@ -16,6 +16,38 @@ app.set("view engine", "ejs");
 mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 
+const articlesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter a title for your article"]
+  },
+
+  content: String
+});
+const Article = mongoose.model("Article", articlesSchema);
+
+
+app.get("/", function (req,res){
+  Article.find({}, function (err, articles){
+    if(err) console.log(err);
+    else{
+
+      if(articles !== undefined && articles.length !== 0)
+        articles.forEach(article => {
+          console.log(article.title);
+        });
+
+      else
+        console.log("empty");
+    }
+  });
+
+  res.render("index");
+});
+
+
+
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
